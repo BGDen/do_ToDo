b2d9ec23f063006260dc943c24ecf8fd33a6996d
Den <fentau.line@gmail.com>
2020-03-07 17:56:00 +0200

    Updates

---
+++ precache-manifest.acef1633939fdefda3942b7658165e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d72900400b7ef8c9568bfe79a54164f",
+    "url": "/do_ToDo/index.html"
+  },
+  {
+    "revision": "2f63c5d0bccb9952713a",
+    "url": "/do_ToDo/static/css/main.92b6ef10.chunk.css"
+  },
+  {
+    "revision": "3046ae8a836faf227a91",
+    "url": "/do_ToDo/static/js/2.5e9415d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/do_ToDo/static/js/2.5e9415d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f63c5d0bccb9952713a",
+    "url": "/do_ToDo/static/js/main.d11eaf2a.chunk.js"
+  },
+  {
+    "revision": "4b8079b8fd23740c23f3",
+    "url": "/do_ToDo/static/js/runtime-main.26d6c28d.js"
+  }
+]);
